10428dae8caba72bc809e495f580b6c11b03690a
Shreya Menon <mnnshreya@gmail.com>
2023-01-24 19:25:51 -0800

    Project2

---
+++ search/.ipynb_checkpoints/graph-checkpoint.py
@@ -0,0 +1,28 @@
+import networkx as nx
+
+class Graph:
+    """
+    Class to contain a graph and your bfs function
+    
+    You may add any functions you deem necessary to the class
+    """
+    def __init__(self, filename: str):
+        """
+        Initialization of graph object 
+        """
+        self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
+
+    def bfs(self, start, end=None):
+        """
+        TODO: write a method that performs a breadth first traversal and pathfinding on graph G
+
+        * If there's no end node input, return a list nodes with the order of BFS traversal
+        * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
+        * If there is an end node input and a path does not exist, return None
+
+        """
+        return
+
+
+
+
